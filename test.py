086ae053ed7f23d960f8b52289779afcb6e71ca2
Seungho Kim <seungho5504@naver.com>
2024-01-24 01:17:04 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,5 @@
+# Title     : Git process understanding
+# Editor    : s2ho.kim
+# Date      : 2024-01-24
+
+print("Hello GitHub!\n")
--- test_git_flow.py
+++
@@ -1,7 +0,0 @@
-# Title     : git hub flow test
-# Date      : 2024-01-24
-# Editor    : s2ho.kim
-# Comment   : first time in git
-# hello
-
-print("Hello Github!\n")
